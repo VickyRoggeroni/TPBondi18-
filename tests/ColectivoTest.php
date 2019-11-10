810aa04ad2aa55019651ef93298ea3101424176a
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-11-10 01:10:57 -0300

    Solo una parte de un test

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -24,9 +24,10 @@ class ColectivoTest extends TestCase
         $this->assertEquals($colectivo->numero(), 37);
         /*
         Probamos la realizacion de una viaje
-         */
+         
         $this->assertEquals($colectivo->pagarCon($tarjeta), new Boleto($colectivo, $tarjeta));
         $this->assertEquals($tarjeta->obtenerSaldo(), 510.15);
+        */
     }
 
     /**
