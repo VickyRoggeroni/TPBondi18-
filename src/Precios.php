8bbaa68390877e80e76eb2629dad5d02ab6f29a6
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-11-26 19:08:34 -0300

    ahora estan los precios del medio, transb y plus

--- src/Precios.php
+++ src/Precios.php
@@ -4,5 +4,8 @@ namespace TrabajoTarjeta;
 
 class Precios
 {
-    public const boleto = 32.5;
+    public const normal = 32.5;
+    public const medio = 16.25;
+    public const transbordo = 0.0;
+    public const plus = 0.0;
 }
