fb21eb0ed4a093734624f90902ec535526c8fda0
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-11-26 18:37:55 -0300

    Cambio funcion de restar saldo y transbordo

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -12,7 +12,7 @@ class Tarjeta implements TarjetaInterface
     protected $UltimaHora = 0;
     protected $UltimoColectivo;
     protected $pagoplus = 0;
-    protected $Ultimotrasbordo = 1;
+    protected $Trasbordo = 0;
     protected $id;
     protected $tiempo;
 
@@ -112,22 +112,27 @@ class Tarjeta implements TarjetaInterface
      */
     public function restarSaldo($linea)
     {
-        $ValorARestar = $this->calculaValor($linea); //Calcula el valor de el boleto
-        if ($this->saldo >= $ValorARestar) { // Si hay saldo
-            $this->saldo -= $ValorARestar; //Se le resta
-            $this->UltimoValorPagado = $ValorARestar; //Se guarda cuanto pago
-            $this->UltimoColectivo = $linea;
-            $this->UltimaHora = $this->tiempo->time(); //Se guarda la hora de la transaccion
-            return true; //Se finaliza la funcion
-        }
-        else if ($this->plus < 2) { //Si tiene plus disponibles
-            $this->plus++; // Se le resta
-            $this->UltimoValorPagado = 0.0; //Se indica que se pago 0.0
-            $this->UltimoColectivo = $linea;
-            $this->UltimaHora = $this->tiempo->time(); //Se almacena la hora de la transaccion
-            return true; // Se finaliza
-        }
-        return false; // No fue posible pagar
+        if( puedeTransbordo() ){
+				
+			$this->UltimoValorPagado = 0.0;
+			$this->UltimoColectivo = $linea;
+			$this->UltimaHora = $this->tiempo->time();
+			return true;
+		}
+		elseif(AlcanzaSaldo()){
+			$this->UltimoValorPagado = 32.5;
+			$this->UltimoColectivo = $linea;
+			$this->UltimaHora = $this->tiempo->time();
+			return true;
+		}
+		elseif(TienePlus()){
+			$this->UltimoValorPagado = 0.0;
+			$this->UltimoColectivo = $linea;
+			$this->UltimaHora = $this->tiempo->time();
+			$this->plus++;
+
+			return true;   
+		}
     }
 
     /**
@@ -139,10 +144,6 @@ class Tarjeta implements TarjetaInterface
      * @return float
      *   El valor del pasaje a pagar.
      */
-    protected function calculaValor($linea)
-    {
-        return ($this->puedeTrasbordo($linea, $this->ValorBoleto));
-    }
 
     /**
      * Funcion para ver si dispone del trasbordo.
@@ -150,58 +151,31 @@ class Tarjeta implements TarjetaInterface
      * @param string $linea
      *   La linea de colectivo en la que se esta pagando, se utiliza para ver si hizo trasbordo.
      *
-     * @param float $ValorBoleto
-     *   El valor del boleto al que se realiza un 33%.
-     *
-     * @return float
-     *   Si fue posible realizar la carga.
+     * @return bool
+     *   Si se puede usar transbordo
      */
-    protected function puedeTrasbordo($linea, $ValorBoleto)
+    protected function puedeTrasbordo($linea)
     {
-        if ($this->UltimoColectivo == $linea || $this->UltimoValorPagado == 0.0 || $this->Ultimotrasbordo) {
-            $this->Ultimotrasbordo = 0;
-            return $ValorBoleto;
-        }
-        if ($this->dependeHora()) {
-            if (($this->tiempo->time() - $this->UltimaHora) < 3600) {
-                $this->Ultimotrasbordo = 1;
-                return ($ValorBoleto * 0.33);
+        if($this->UltimoColectivo != $linea || $this->UltimoValorPagado != 0.0 || $this->transbordo != 1){   //Se fija condiciones
+            if ($this->Tiempo->EsFeriado()){
+                return (($this->tiempo->time() - $this->UltimaHora) < 7200);
+            }            
+
+            elseif ($this->Tiempo->esDiaDeSemana()){
+                if(($this->Tiempo->EsDeNoche()) == false ){//Si no es de noche, es de dia :)
+                    return (($this->tiempo->time() - $this->UltimaHora) < 3600);                             //Si paso menos de una hora devuelve true
+                }
+                if($this->Tiempo->EsDeNoche()){
+                    return (($this->tiempo->time() - $this->UltimaHora) < 7200);
+                }
             }
-        } else {
-            if (($this->tiempo->time() - $this->UltimaHora) < 5400) {
-                $this->Ultimotrasbordo = 1;
-                return ($ValorBoleto * 0.33);
-            }
+
+            else return(($this->tiempo->time() - $this->UltimaHora) < 7200);                                 //si no es feriado o dia de semana, es finde, entonces son dos horas el transb
         }
-        $this->Ultimotrasbordo = 0;
-        return $ValorBoleto;
+
+        else return false;                                                                                  //Si no cumple las condiciones devuelve false 
     }
 
-    /**
-     * Dependiendo de la hora y el dia que sea puede haber un maximo de tiempo de 60 o 90 minutos.
-     *
-     * @return bool
-     *   True si son 60 o false si son 90.
-     */
-    protected function dependeHora()
-    {
-        if ($this->tiempo->esFeriado() || date('N', $this->tiempo->time()) == 7){
-            return false;
-        }
-        if (date('N', $this->tiempo->time()) == 6){
-            if (date('G', $this->tiempo->time()) > 6 && date('G', $this->tiempo->time()) < 14){
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            if (date('G', $this->tiempo->time()) > 6 && date('G', $this->tiempo->time()) < 22){
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
 
     /**
      * Setea a 0 el "pago plus". Esta funcion se ejecutara cuando se emite el boleto.
