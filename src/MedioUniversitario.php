54a6972a18d7eea2b9f5d8f3047b2217162b74be
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-11-27 01:23:58 -0300

    Actualice restarSaldo de acuerdo a los cambios previos

--- src/MedioUniversitario.php
+++ src/MedioUniversitario.php
@@ -4,32 +4,60 @@ namespace TrabajoTarjeta;
 class MedioUniversitario extends Medio
 {
     protected $DisponiblesDiarios = 0; //Variable que indica la disponibilidad de medios diarios
+    protected $ValorBoleto = Precios::medio;
 
     public function franquicia()
     {
         return 2; //devuelve 2 si es Trasbordo
     }
+    
+    public function restarSaldo($linea){
 
-    /**
-     * Devuelve el valor del boleto a pagar, pero antes se fija si puede hacer un trasbordo utilizando al otra funcion y si es uno de los 2 medios diarios que dispone.
-     *
-     * @param string $linea
-     *   La linea en la que esta intentando pagar.
-     *
-     * @return float
-     *   El valor del boleto a pagar.
-     */
-    protected function calculaValor($linea)
-    {
-        $BoletoTemporal = 0;
+        if( puedeTransbordo($linea) ){
+				
+			$this->UltimoValorPagado = Precios::transbordo; //guarda el ultimo valor
+			$this->UltimoColectivo = $linea; //guarda el ultimo colectivo
+			$this->UltimaHora = $this->tiempo->time(); //guarda la ultima hora
+            $this->transbordo = 1; //Marca que el transbordo ya fue usado
+			return true;
+		}
+		elseif(TieneMedioDisponible()){
+			$this->UltimoValorPagado = $ValorBoleto;
+			$this->UltimoColectivo = $linea;
+			$this->UltimaHora = $this->tiempo->time();
+            $this->saldo -= $ValorBoleto;
+			return true;
+		}
+        elseif(AlcanzaSaldo()){
+			$this->UltimoValorPagado = Precios::normal;
+			$this->UltimoColectivo = $linea;
+			$this->UltimaHora = $this->tiempo->time();
+            $this->saldo -= Precios::normal;				//Se resta el boleto
+			return true;
+		}
+
+		elseif(TienePlus()){
+			$this->UltimoValorPagado = Precios::plus;
+			$this->UltimoColectivo = $linea;
+			$this->UltimaHora = $this->tiempo->time();
+			$this->plus++;
+			return true;   
+		}
+
+        else return false;       //No se pudo restar el saldo
+}
+
+public function TieneMedioDisponible(){
         $UltimaFecha = date("d/m/y", $this->UltimaHora); //Guarda Cuando fue la ultima utilizacion del boleto
         $ActualFecha = date("d/m/y", $this->tiempo->time()); //Guarda la hora actual
-        if ($ActualFecha > $UltimaFecha) {$this->DisponiblesDiarios = 0;} // Si cambio de dia entre la utilizacion anterior se reinicia la disponibilidad
+        if ($ActualFecha > $UltimaFecha) {
+            $this->DisponiblesDiarios = 0;
+        } //resetea cantidad de medios disponibles por dia
+
         if ($this->DisponiblesDiarios < 2) { //Si dispone de Medios
             $this->DisponiblesDiarios++; //Le saca uno
-            $BoletoTemporal = (Precios::medio); //Y devualve la mitad del valor
-        } else {$BoletoTemporal = Precios::normal;} // Devuelve el valor entero
-
-        return ($this->puedeTrasbordo($linea,$BoletoTemporal));
-    }
+            return true;
+        }
+        else return false;
+}
 }
