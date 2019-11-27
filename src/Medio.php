b472b54a6a7886201addd44fb7ced225118cf49f
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-11-27 00:47:51 -0300

    Cambios en restarSaldo

--- src/Medio.php
+++ src/Medio.php
@@ -1,4 +1,4 @@
-<?php
++<?php
 namespace TrabajoTarjeta;
 
 /*/
@@ -8,7 +8,7 @@ class Medio extends Tarjeta
 {
 
     protected $UltimaHora = -300; //Para poder usarlo apenas se compra
-
+    protected $ValorBoleto = Precios::medio;
     public function franquicia()
     {
         return 1; //devuelve 1 si es Medio
@@ -25,36 +25,32 @@ class Medio extends Tarjeta
      */
     public function restarSaldo($linea)
     {
-        if (($this->tiempo->time() - $this->UltimaHora) < 299) {return false;} //Limitacion de 5 minutos
-        $ValorARestar = $this->calculaValor($linea); //Llama a la funcion que calcula el valor del boleto a pagar
-        if ($this->saldo >= $ValorARestar) { //Se fija si le alcanza el saldo
-            $this->saldo -= $ValorARestar; //En caso de alcanzar lo resta
-            $this->UltimoValorPagado = $ValorARestar; //Guarda el valor del ultimo pago que se realizo
-            $this->UltimoColectivo = $linea;
-            $this->UltimaHora = $this->tiempo->time(); //Guarda la hora de este pago
-            return true; //se completa el pago
-        }
-        if ($this->plus < 2) { //En caso de no alcanzarle el saldo, se fija si dispone de plus
-            $this->plus++; //le saca un plus
-            $this->UltimaHora = $this->tiempo->time(); //Guarda la hora de utilizacion del plus
-            $this->UltimoColectivo = $linea;
-            $this->UltimoValorPagado = 0.0; //Indica que se pago 0.0
-            return true; //Completa el pago
-        }
+        if (($this->tiempo->time() - $this->UltimaHora) < 299) {
+            return false;
+        } //Limitacion de 5 minutos
+        
+        if( puedeTransbordo($linea) ){
+				
+			$this->UltimoValorPagado = Precios::transbordo;
+			$this->UltimoColectivo = $linea;
+			$this->UltimaHora = $this->tiempo->time();
+            $this->transbordo = 1;
+			return true;
+		}
+		elseif($this->saldo >= $this->ValorBoleto){
+			$this->UltimoValorPagado = Precios::medio;
+			$this->UltimoColectivo = $linea;
+			$this->UltimaHora = $this->tiempo->time();
+            $this->saldo -= $ValorBoleto;
+			return true;
+		}
+		elseif(TienePlus()){
+			$this->UltimoValorPagado = Precios::plus;
+			$this->UltimoColectivo = $linea;
+			$this->UltimaHora = $this->tiempo->time();
+			$this->plus++;
+			return true;   
+		}
         return false; //No Se pudo pagar
-    }
-
-    /**
-     * Devuelve el valor del boleto a pagar, pero antes se fija si puede hacer un trasbordo utilizando al otra funcion.
-     *
-     * @param string $linea
-     *   La linea en la que esta intentando pagar.
-     *
-     * @return float
-     *   El valor del boleto a pagar.
-     */
-    protected function calculaValor($linea)
-    {
-        return ($this->puedeTrasbordo($linea, ($this->boleto / 2)));
     }
 }
