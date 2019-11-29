5829a17a9ebf4314030c2f9ace96cb7a44cbb308
Glonder <emrcnyrc@hotmail.com>
2019-11-28 23:16:48 -0300

    Arreglos

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -21,6 +21,7 @@ class ColectivoTest extends TestCase
         $this->assertEquals($colectivo->numero(), 37);
         /*
         Probamos la realizacion de una viaje
+        */
          
         $this->assertEquals($colectivo->pagarCon($tarjeta), new Boleto($colectivo, $tarjeta));
 
@@ -30,19 +31,18 @@ class ColectivoTest extends TestCase
 
     /**
      * Probamos la realizacion de un pago sin saldo y el uso de plus
+     */
    
     public function testSinSaldo()
     {
-
         $tiempo = new Tiempo;
         $tarjeta = new Tarjeta(0, $tiempo);
         $colectivo = new Colectivo(141, "Semtur", 37);
         
-        Probamos la realizacion de una viaje sin saldo
+        /*Probamos la realizacion de una viaje sin saldo*/
          
         $this->assertEquals($colectivo->pagarCon($tarjeta), new Boleto($colectivo, $tarjeta));
         $this->assertEquals($colectivo->pagarCon($tarjeta), new Boleto($colectivo, $tarjeta));
-        $this->assertEquals($colectivo->pagarCon($tarjeta), false);
+        $this->assertEquals($colectivo->pagarCon($tarjeta), false); 
     }
-    */
 }
