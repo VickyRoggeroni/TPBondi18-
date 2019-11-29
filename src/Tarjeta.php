4435f920bcb29f0f05bb03c9e0d66c4b936977c0
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-11-28 21:07:06 -0300

    pagarPlus tenia que ser public

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -16,7 +16,7 @@ class Tarjeta implements TarjetaInterface
     protected $id;
     protected $tiempo;
 
-    public function __construct($id, TiempoInterface $tiempo)
+    public function __construct($id, Tiempo $tiempo)
     {
         $this->id = $id; //Guarda el ID
         $this->tiempo = $tiempo; //Guarda la variable tiempo la cual le es inyectada
@@ -33,7 +33,7 @@ class Tarjeta implements TarjetaInterface
      * @return bool
      *   Si fue posible realizar la carga.
      */
-    public function recargar($monto)
+    public function recargar(float $monto)
     {
 
         switch ($monto) { //Diferentes montos a recargar
@@ -70,19 +70,21 @@ class Tarjeta implements TarjetaInterface
     /**
      * Funcion para pagar plus en caso de deberlos.
      */
-    protected function pagarPlus()
+    public function pagarPlus()
     {
         if ($this->plus == 2) { //Si debe 2 plus
             if ($this->saldo >= ($this->ValorBoleto * 2)) { //Y si le alcanza el saldo para pagarlos
                 $this->saldo -= ($this->ValorBoleto * 2); //Se le resta el valor
                 $this->plus = 0; //Se le devuelve los plus
                 $this->pagoplus = 2; //Se almacena que se pagaron 2 plus
-            } else if ($this->saldo >= $this->ValorBoleto) { // Si solo alcanza para 1 plus
+            }
+	    else if ($this->saldo >= $this->ValorBoleto) { // Si solo alcanza para 1 plus
                 $this->saldo -= $this->ValorBoleto; //se le descuenta
                 $this->plus = 1; // Se lo devuelve
                 $this->pagoplus = 1; // Se indica que se pago un plus
             }
-        } else {
+        } 
+	else {
             if ($this->plus == 1 && $this->saldo > $this->ValorBoleto) { //si debe 1 plus
                 $this->saldo -= $this->ValorBoleto; //Se le descuenta
                 $this->plus = 0; //Se le devuelve
