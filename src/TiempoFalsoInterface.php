cab977fa1ed22b63bf6c18aef62c012468f3515d
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-11-30 01:25:32 -0300

    funciones esDiaDeSemana y EsDeNoche

--- src/TiempoFalsoInterface.php
+++ src/TiempoFalsoInterface.php
@@ -8,4 +8,6 @@ interface TiempoFalsoInterface
     public function time();
     public function agregarFeriado($dia);
     public function esFeriado();
+    public function esDiaDeSemana();    
+    public function EsDeNoche();
 }
