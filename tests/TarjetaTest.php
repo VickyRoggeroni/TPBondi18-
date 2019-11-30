9402bece30321ba8084c5e730b7328f58093a95e
Glonder <emrcnyrc@hotmail.com>
2019-11-29 22:19:05 -0300

    EsFeriado

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -185,7 +185,7 @@ class TarjetaTest extends TestCase
     {
         $tiempo = new Tiempo();
         $tiempo->agregarFeriado("01-01-20");
-        $this->AssertFalse($tiempo->esFeriado());
+        $this->AssertFalse($tiempo->EsFeriado());
         $tarjeta = new Tarjeta(0, $tiempo);
         $tarjeta->recargar(100);
         $tarjeta->recargar(100);
@@ -205,7 +205,7 @@ class TarjetaTest extends TestCase
     {
         $tiempo = new Tiempo();
         $tiempo->agregarFeriado("01-01-18");
-        $this->AssertFalse($tiempo->esFeriado());
+        $this->AssertFalse($tiempo->EsFeriado());
         $tarjeta = new Tarjeta(0, $tiempo);
         $tarjeta->recargar(100);
         $tarjeta->recargar(100);
