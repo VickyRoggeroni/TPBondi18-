51efd7ef0ff8b0f14713a553b5e9107feaeea66b
VickyRoggeroni <49886020+VickyRoggeroni@users.noreply.github.com>
2019-12-01 17:08:54 -0300

    Formato (y un par de detalles)

--- src/Transbordo.php
+++ src/Transbordo.php
@@ -15,17 +15,13 @@ trait Transbordo
      */
     public function puedeTransbordo($linea)
     {
+	//           Primer viaje                   Viaje plus                    Misma linea
         if ($this->UltimoColectivo == null || $this->TipoBoleto == 2 || $this->UltimoColectivo == $linea)
         {
 		return false;
         }
 	    
-        if($this->TipoBoleto != 2 && $this->transbordo == 1)
-        { //Si ya se uso un transbordo, pero despues se volvio a usar la tarjeta
-                $this->transbordo = 0; //Se va a resetear el transbordo
-        }
-	    
-        if($this->UltimoColectivo != $linea || $this->TipoBoleto != 2 || $this->transbordo != 1)
+        if($this->UltimoColectivo != $linea || $this->TipoBoleto != 2)
         {   //Se fija condiciones
             if ($this->tiempo->EsFeriado())
             {
@@ -35,15 +31,15 @@ trait Transbordo
             {
                 if(($this->tiempo->EsDeNoche()) == false )
                 {//Si no es de noche, es de dia :)
-                    return (($this->tiempo->time() - $this->UltimaHora) < 3600);                             //Si paso menos de una hora devuelve true
+                    return (($this->tiempo->time() - $this->UltimaHora) < 3600);           //Si paso menos de una hora devuelve true
                 }
                 if($this->tiempo->EsDeNoche())
                 {
                     return (($this->tiempo->time() - $this->UltimaHora) < 7200);
                 }
             }
-            else return(($this->tiempo->time() - $this->UltimaHora) < 7200);                                 //si no es feriado o dia de semana, es finde, entonces son dos horas el transb
+            else return(($this->tiempo->time() - $this->UltimaHora) < 7200);                //si no es feriado o dia de semana, es finde, entonces son dos horas el transb
         }
-        else return false;                                                                                  //Si no cumple las condiciones devuelve false 
+        else return false;                                                                  //Si no cumple las condiciones devuelve false 
     }
 }
