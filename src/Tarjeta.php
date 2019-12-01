e9f0e72b746c6ca2b3dd8c46b1ce59f7ccb4b28b
Glonder <emrcnyrc@hotmail.com>
2019-11-30 23:06:04 -0300

    Transbordo

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -108,7 +108,7 @@ class Tarjeta implements TarjetaInterface
      */
     public function restarSaldo($linea)
     {
-        if ($this->puedeTransbordo($linea)){
+        if (Transbordo::puedeTransbordo($linea)){
 		$this->TipoBoleto = 0;
 		$this->transbordo = 1;
 		$this->PagoExitoso = true;
@@ -146,50 +146,21 @@ class Tarjeta implements TarjetaInterface
 		return $this->PagoExitoso;
 	}
     }
-    /**
-     * Funcion para ver si dispone del trasbordo.
-     *
-     * @param string $linea
-     *   La linea de colectivo en la que se esta pagando, se utiliza para ver si hizo trasbordo.
-     *
-     * @return bool
-     *   Si se puede usar transbordo
-     */
-    public function puedeTransbordo($linea)
-    {
-        if ($this->UltimoColectivo == null || $this->TipoBoleto == 2 || $this->UltimoColectivo == $linea){
-		return false;
-	}
-	    
-	if($this->TipoBoleto != 2 && $this->transbordo == 1){ //Si ya se uso un transbordo, pero despues se volvio a usar la tarjeta
-            $this->transbordo = 0; //Se va a resetear el transbordo
-        }
-	    
-	if($this->UltimoColectivo != $linea || $this->TipoBoleto != 2 || $this->transbordo != 1){   //Se fija condiciones
-            if ($this->tiempo->EsFeriado()){
-                return (($this->tiempo->time() - $this->UltimaHora) < 7200);
-            }            
-            elseif ($this->tiempo->esDiaDeSemana()){
-                if(($this->tiempo->EsDeNoche()) == false ){//Si no es de noche, es de dia :)
-                    return (($this->tiempo->time() - $this->UltimaHora) < 3600);                             //Si paso menos de una hora devuelve true
-                }
-                if($this->tiempo->EsDeNoche()){
-                    return (($this->tiempo->time() - $this->UltimaHora) < 7200);
-                }
-            }
-            else return(($this->tiempo->time() - $this->UltimaHora) < 7200);                                 //si no es feriado o dia de semana, es finde, entonces son dos horas el transb
-        }
-        else return false;                                                                                  //Si no cumple las condiciones devuelve false 
-    }
+
+
+
+
+
+
     public function AlcanzaSaldo(){                                //Si el saldo es mayor o igual al valor del boleto, entonces le alcanza para pagarlo
     	
-	if ($this->TipoBoleto == 2){ 			  	   //Si el ultimo boleto usado fue un plus no se puede usar el saldo comun
-		return false;
-	}
-	    
-	else if($this->saldo >= $this->ValorBoleto){
-		return true;
-	}
+        if ($this->TipoBoleto == 2){ 			  	   //Si el ultimo boleto usado fue un plus no se puede usar el saldo comun
+            return false;
+        }
+            
+        else if($this->saldo >= $this->ValorBoleto){
+            return true;
+        }
     }
 	
     public function TienePlus(){                                   //Si uso menos de dos plus devuelve true
